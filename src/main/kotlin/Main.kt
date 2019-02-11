e17ce4de169c6be3ba962f768cb121d9c74a8c85
Aghasy <agasy18@gmail.com>
2019-02-11 14:12:32 +0400

    Bug fix in test app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,14 @@ fun main() {
         try {
             println(eval(it))
         } catch (e: IllegalExpressionException) {
-            println("Error '${e.message} | ${it.substring(0, e.index)} >>${it[e.index]}<<< ${it.substring(e.index)}")
+            println(
+                "Error '${e.message} | ${it.substring(0, e.index)}${
+                if (e.index < it.length)
+                    " >> ${it.substring(e.index, e.index + 1)} << ${it.substring(e.index)}"
+                else
+                    "??"
+                }"
+            )
         }
     }
 }
